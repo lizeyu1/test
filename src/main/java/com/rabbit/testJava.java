5497ef391882b7ab8480a113b33a76c4f43d4fcf
zhengqiangjun <641249208@qq.com>
2019-05-18 21:28:01 +0800

    commit 01

---
+++ src/main/java/com/rabbit/testJava.java
@@ -0,0 +1,15 @@
+package com.rabbit;
+
+/**
+ * @Auther: zhengqiangjun
+ * @Date: 2019/5/18 21:14
+ * @Description:
+ */
+public class testJava {
+
+    public static void main(String[] args){
+
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/main/scala/com/rabbit/testScala.scala
@@ -0,0 +1,13 @@
+package com.rabbit
+
+/**
+  * @Auther: zhengqiangjun
+  * @Date: 2019/5/18 21:14
+  * @Description:
+  */
+object testScala {
+    def main(args: Array[String]): Unit = {
+        println("hello world")
+    }
+
+}
